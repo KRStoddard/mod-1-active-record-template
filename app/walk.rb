d921f1b224f1845399fb30819c6ce339d976804d
KRStoddard <kimberlynrstoddard@gmail.com>
2020-08-14 15:51:15 -0500

    Done

---
+++ app/walk.rb
@@ -0,0 +1,12 @@
+class Walk < ActiveRecord::Base 
+    belongs_to :dog
+    belongs_to :dog_walker
+
+    def happy_walker?
+        self.dog.breed == self.dog_walker.favorite_breed
+    end
+
+    def print_details
+        puts "#{self.dog_walker.name} walked #{self.dog.name} for #{self.length_in_minutes} minutes."
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,150 @@
 # create seed data here!
 
+Dog.destroy_all
+DogWalker.destroy_all 
+Walk.destroy_all 
+
+Dog.create({
+    name: Faker::Creature::Dog.name,
+    breed: Faker::Creature::Dog.breed,
+    good_dog: true}
+)
+Dog.create({
+    name: Faker::Creature::Dog.name,
+    breed: Faker::Creature::Dog.breed,
+    good_dog: true}
+)
+Dog.create({
+    name: Faker::Creature::Dog.name,
+    breed: Faker::Creature::Dog.breed,
+    good_dog: true}
+)
+Dog.create({
+    name: Faker::Creature::Dog.name,
+    breed: Faker::Creature::Dog.breed,
+    good_dog: true}
+)
+Dog.create({
+    name: Faker::Creature::Dog.name,
+    breed: Faker::Creature::Dog.breed,
+    good_dog: true}
+)
+
+DogWalker.create({
+    name: Faker::Name.name,
+    favorite_breed: Faker::Creature::Dog.breed
+})
+
+DogWalker.create({
+    name: Faker::Name.name,
+    favorite_breed: Faker::Creature::Dog.breed
+})
+
+DogWalker.create({
+    name: Faker::Name.name,
+    favorite_breed: Faker::Creature::Dog.breed
+})
+
+DogWalker.create({
+    name: Faker::Name.name,
+    favorite_breed: Faker::Creature::Dog.breed
+})
+
+DogWalker.create({
+    name: Faker::Name.name,
+    favorite_breed: Faker::Creature::Dog.breed
+})
+
+DogWalker.create({
+    name: Faker::Name.name,
+    favorite_breed: Faker::Creature::Dog.breed
+})
+
+DogWalker.create({
+    name: Faker::Name.name,
+    favorite_breed: Faker::Creature::Dog.breed
+})
+
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+Walk.create({
+    length_in_minutes: rand(1..120),
+    dog_id: rand(Dog.first.id..Dog.last.id),
+    dog_walker_id: rand(DogWalker.first.id..DogWalker.last.id)
+})
+
+
+
+
 
 
 puts "Done 🌱"
